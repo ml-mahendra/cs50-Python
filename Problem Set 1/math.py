d3a6d155c6d8a738bf58b09ea06449ecbeddd804
ml-mahendra <ml.mahendranath@gmail.com>
2023-12-26 15:36:35 +0000

    Problem Set 1

---
+++ Problem Set 1/math.py
@@ -0,0 +1,17 @@
+exp = input("Expression: ")
+x,y, z = exp.split(" ")
+x= int(x)
+z=int(z)
+match y:
+    case '+':
+        print(float(x+z))
+
+    case '-':
+        print(float(x-z))
+
+    case '*':
+        print(float(x*z))
+
+    case '/':
+        print(float(x/z))
+
